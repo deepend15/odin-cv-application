d7aff84678c110e0b5d079ea73ed3c1d6ecb3edc
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-10-01 16:55:34 -0400

    Organize files & add skeleton code

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Resume Builder</h1>
+      <form action="" method="">
+        {/* General component */}
+        {/* Education component */}
+        {/* Experience component */}
+        <button>Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default App
