3334fc0d50fa0b46f8ff8a01446a808dfdd37489
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-10-17 17:26:31 -0400

    Display add'l schools / companies
    Display all schools / companies in final resume display if user has
    added any additional

--- src/components/ResumeDisplay.jsx
+++ src/components/ResumeDisplay.jsx
@@ -1,16 +1,8 @@
 import "../styles/ResumeDisplay.css";
 
 export default function ResumeDisplay({ formFields, handleEdit }) {
-  let resumeDegreeValue;
-  formFields.School1Degree.value === "diploma"
-    ? (resumeDegreeValue = "Diploma / GED")
-    : formFields.School1Degree.value === "associate"
-    ? (resumeDegreeValue = "Associate's Degree")
-    : formFields.School1Degree.value === "bachelor"
-    ? (resumeDegreeValue = "Bachelor's Degree")
-    : formFields.School1Degree.value === "master"
-    ? (resumeDegreeValue = "Master's Degree / Equivalent")
-    : (resumeDegreeValue = "Doctorate / Equivalent");
+  const schoolIdsArray = formFields.Education.childIds;
+  const companyIdsArray = formFields.Experience.childIds;
 
   return (
     <>
@@ -22,7 +14,45 @@ export default function ResumeDisplay({ formFields, handleEdit }) {
         </section>
         <section className="education-section">
           <h2>Education</h2>
-          <h3>{formFields.School1Name.value}</h3>
+          {schoolIdsArray.map((id) => {
+            const formFieldArray = Object.entries(formFields);
+            function getField(string) {
+              const targetedFieldArray = formFieldArray.filter(
+                (field) => field[1].id === id + string
+              );
+              const targetedFieldName = targetedFieldArray[0][0];
+              return formFields[targetedFieldName];
+            }
+            const schoolDegreeField = getField("Degree");
+            let resumeDegreeValue;
+            schoolDegreeField.value === "diploma"
+              ? (resumeDegreeValue = "Diploma / GED")
+              : schoolDegreeField.value === "associate"
+              ? (resumeDegreeValue = "Associate's Degree")
+              : schoolDegreeField.value === "bachelor"
+              ? (resumeDegreeValue = "Bachelor's Degree")
+              : schoolDegreeField.value === "master"
+              ? (resumeDegreeValue = "Master's Degree / Equivalent")
+              : (resumeDegreeValue = "Doctorate / Equivalent");
+
+            return (
+              <div key={id}>
+                <h3>{getField("Name").value}</h3>
+                <p>{getField("Location").value}</p>
+                <p>
+                  {getField("StartMonth").value} {getField("StartYear").value} -{" "}
+                  {getField("EndMonth").value} {getField("EndYear").value}
+                </p>
+                <p>
+                  {resumeDegreeValue}
+                  {schoolDegreeField.value !== "diploma" && (
+                    <>: {getField("FieldOfStudy").value}</>
+                  )}
+                </p>
+              </div>
+            );
+          })}
+          {/* <h3>{formFields.School1Name.value}</h3>
           <p>{formFields.School1Location.value}</p>
           <p>
             {formFields.School1StartMonth.value}{" "}
@@ -34,11 +64,34 @@ export default function ResumeDisplay({ formFields, handleEdit }) {
             {formFields.School1Degree.value !== "diploma" && (
               <>: {formFields.School1FieldOfStudy.value}</>
             )}
-          </p>
+          </p> */}
         </section>
         <section className="experience-section">
           <h2>Work Experience</h2>
-          <h3>{formFields.Company1Name.value}</h3>
+          {companyIdsArray.map((id) => {
+            const formFieldArray = Object.entries(formFields);
+            function getField(string) {
+              const targetedFieldArray = formFieldArray.filter(
+                (field) => field[1].id === id + string
+              );
+              const targetedFieldName = targetedFieldArray[0][0];
+              return formFields[targetedFieldName];
+            }
+
+            return (
+              <div key={id}>
+                <h3>{getField("Name").value}</h3>
+                <p>{getField("Location").value}</p>
+                <p className="position-title">{getField("Position").value}</p>
+                <p className="work-year">
+                  {getField("StartMonth").value} {getField("StartYear").value} -{" "}
+                  {getField("EndMonth").value} {getField("EndYear").value}
+                </p>
+                <pre>{getField("Responsibilities").value}</pre>
+              </div>
+            );
+          })}
+          {/* <h3>{formFields.Company1Name.value}</h3>
           <p>{formFields.Company1Location.value}</p>
           <p className="position-title">{formFields.Company1Position.value}</p>
           <p className="work-year">
@@ -47,7 +100,7 @@ export default function ResumeDisplay({ formFields, handleEdit }) {
             {formFields.Company1EndMonth.value}{" "}
             {formFields.Company1EndYear.value}
           </p>
-          <pre>{formFields.Company1Responsibilities.value}</pre>
+          <pre>{formFields.Company1Responsibilities.value}</pre> */}
         </section>
       </main>
       <button onClick={handleEdit} className="edit-button">
